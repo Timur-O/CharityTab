92a5b323a67f9e907b0c574b09e52ea95127394c
Timur <19150752+Timur-O@users.noreply.github.com>
2020-05-24 19:22:00 +0200

    Add analytics file

---
+++ analytics.js
@@ -0,0 +1,11 @@
+// Google Analytics
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'VIEWID']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
